c22bb173872834b8f5871258b2d115deaaec18d1
845593 <845593@SAIT227780IT.ACDM.DS.SAIT.CA>
2022-02-07 19:55:22 -0700

    modify BikeDB

--- src/main/java/dataaccess/BikeDB.java
+++ src/main/java/dataaccess/BikeDB.java
@@ -6,8 +6,11 @@
 package dataaccess;
 
 import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 import models.Bike;
+import models.User;
 
 /**
  *
@@ -16,41 +19,116 @@ import models.Bike;
 public class BikeDB extends CommonDB<Bike> {
 
     private final static BikeDB bikeDB = new BikeDB();
-    private BikeDB(){
-    super();
+
+    private BikeDB() {
+        super();
     }
+
     public ArrayList<Bike> getAll() {
         EntityManager em = DBUtil.getEmFactory().createEntityManager();
 
-        return null;
+        try {
+            List<Bike> lists;
+            lists = em.createNamedQuery("Bike.findAll", Bike.class).getResultList();
+            return (ArrayList<Bike>) lists;
+        } finally {
+            em.close();
+        }
 
     }
 
     public final ArrayList<Bike> getAll(String cat) {
         EntityManager em = DBUtil.getEmFactory().createEntityManager();
 
-        return null;
+        try {
+            List<Bike> lists;
+            lists = em.createNamedQuery("Bike.findByCategory", Bike.class).getResultList();
+            return (ArrayList<Bike>) lists;
+        } finally {
+            em.close();
+        }
 
     }
 
     public final Bike getBike(int id) {
-        return null;
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        try {
+            //System.out.println("Category get : "+name);
+
+            Bike bike = em.createNamedQuery("Bike.findById", Bike.class).setParameter("id", id).getSingleResult();
+            return bike;
+        } finally {
+            em.close();
+        }
 
     }
 
     @Override
     public boolean add(Bike a) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  EntityManager em = DBUtil.getEmFactory().createEntityManager();
+             EntityTransaction trans=em.getTransaction();
+        try {
+            //System.out.println("userDB update       "+ email+"   "+status+"     "+role+"     "+email+"     "+firstName);
+            
+             
+            User user = a.getOwner();
+            user.getBikes().add(a);
+             trans.begin();
+             em.persist(a);
+             em.merge(user);
+           
+             trans.commit();
+             return true;
+          
+        } catch(Exception ex){
+        trans.rollback();
+        return false;
+        } finally {
+            em.close();
+        }    
     }
 
     @Override
     public boolean update(Bike a) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        try {
+            //System.out.println("userDB update       "+ email+"   "+status+"     "+role+"     "+email+"     "+firstName);
+
+            trans.begin();
+            em.merge(a);
+
+            trans.commit();
+            return true;
+
+        } catch (Exception ex) {
+            trans.rollback();
+            return false;
+        } finally {
+            em.close();
+        }
     }
 
     @Override
     public boolean delete(Bike a) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                       EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        
+        try {
+            User user = a.getOwner();
+            user.getBikes().remove(a);
+            trans.begin();
+            em.remove(em.merge(a));
+            em.merge(user);
+            trans.commit();
+            return true;
+        } catch (Exception ex) {
+            trans.rollback();
+            return false;
+        } finally {
+            em.close();
+        
+        }   
     }
 
     public static BikeDB getInstance() {
