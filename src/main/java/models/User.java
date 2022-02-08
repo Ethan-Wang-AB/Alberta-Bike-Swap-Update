5fad28a9f868fa666a0828cba3f277366e972636
845593 <845593@SAIT227780IT.ACDM.DS.SAIT.CA>
2022-02-07 19:56:10 -0700

    modify BikeDB, add getter and setters to User

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -22,5 +22,87 @@ public class User {
     private Role role;     
     private ArrayList<Bike> bikes;      
     private long ticket;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public ArrayList<Bike> getBikes() {
+        return bikes;
+    }
+
+    public void setBikes(ArrayList<Bike> bikes) {
+        this.bikes = bikes;
+    }
+
+    public long getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(long ticket) {
+        this.ticket = ticket;
+    }
+    
+    
     
 }
