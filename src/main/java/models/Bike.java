c88ab9d622bb1b7dbed46d67e464506484963b81
845593 <845593@SAIT227780IT.ACDM.DS.SAIT.CA>
2022-02-07 19:56:27 -0700

    modify BikeDB, add getter and setters to Bike

--- src/main/java/models/Bike.java
+++ src/main/java/models/Bike.java
@@ -20,4 +20,87 @@ public class Bike {
     private boolean donate;     
     private boolean toSell;     
     private String photoPath; 
+
+    public int getBikeId() {
+        return bikeId;
+    }
+
+    public void setBikeId(int bikeId) {
+        this.bikeId = bikeId;
+    }
+
+    public long getIndex() {
+        return index;
+    }
+
+    public void setIndex(long index) {
+        this.index = index;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public void setMaker(String maker) {
+        this.maker = maker;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public boolean isDonate() {
+        return donate;
+    }
+
+    public void setDonate(boolean donate) {
+        this.donate = donate;
+    }
+
+    public boolean isToSell() {
+        return toSell;
+    }
+
+    public void setToSell(boolean toSell) {
+        this.toSell = toSell;
+    }
+
+    public String getPhotoPath() {
+        return photoPath;
+    }
+
+    public void setPhotoPath(String photoPath) {
+        this.photoPath = photoPath;
+    }
+    
+    
+     
 }
