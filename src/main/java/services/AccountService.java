0384054ba6fc49239bcfacbc782d9005e3129662
845593 <845593@host.docker.internal>
2022-02-12 10:48:07 -0700

    add getByEmail getByUsername, getByUUID method in accountservice, modify Bikecheckservelts--move processrequest content to doGet Add content to picture servlet delete some documentation template in LoginServlet

--- src/main/java/services/AccountService.java
+++ src/main/java/services/AccountService.java
@@ -13,17 +13,18 @@ import models.User;
  * @author 845593
  */
 public class AccountService {
-      
-    private UserDB userDB=UserDB.getInstance();
-    public final User login(String username, String password)
-    {
-	User user=userDB.getUser(username);
-        if(user.getPassword().equals(password)){
-        return user;
+
+    private UserDB userDB = UserDB.getInstance();
+
+    public final User login(String username, String password) {
+        User user = userDB.getUser(username);
+        if (user.getPassword().equals(password)) {
+            return user;
+        } else {
+            return null;
         }
-        else return null;
-    }    
-    
+    }
+
     /*
      * logout function will be implemented in servlet side, no need to call method here to talk with database
      * @param user 
@@ -32,17 +33,15 @@ public class AccountService {
 //    {
 //       
 //    }    
-    
     /**
-     * use uuid to reset password. when user reset password, system will generate a uuid and send by email,
-     * client will use uuid uri to open the reset password page, if uuid matches, reset password, if not, resetting fails.
+     * use uuid to reset password. when user reset password, system will generate a uuid and send by email, client will use uuid uri to open the reset password page, if uuid matches, reset password, if not, resetting fails.
+     *
      * @param password
      * @param uuid
-     * @return 
+     * @return
      */
-    public final boolean resetpassword(String password, String uuid)
-    {
-           
+    public final boolean resetpassword(String password, String uuid) {
+
         try {
             User user = userDB.getByUUID(uuid);
             //System.out.println(user.getEmail());
@@ -54,15 +53,36 @@ public class AccountService {
         } catch (Exception ex) {
             return false;
         }
-    }    
+    }
+
+    public User getByEmail(String email) {
+
+        return userDB.getUserByEmail(email);
+
+    }
+
+    public User getByUsername(String username) {
+
+        return userDB.getUser(username);
+
+    }
+
     
-    
-    public final void update(User user)
-    {
+    /**
+     * used for resetting password credential validation
+     * @param uuid
+     * @return the user with matched uuid or null if no match found
+     */
+    public User getByUUID(String uuid) {
+
+        return userDB.getByUUID(uuid);
+
+    }
+
+    public final void update(User user) {
         userDB.update(user);
-    }    
-    
-    
+    }
+
 //    public final void deactive(User user)
 //    {
 //        
--- src/main/java/servlets/PictureServlet.java
+++ src/main/java/servlets/PictureServlet.java
@@ -5,58 +5,68 @@
  */
 package servlets;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.User;
+import services.AccountService;
 
 /**
  *
  * @author 845593
  */
 public class PictureServlet extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet PictureServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet PictureServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+      
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+         try {
+        ServletContext cntx = request.getServletContext();
+        // Get the absolute path of the image
+        HttpSession session = request.getSession();
+        String email = (String) session.getAttribute("email");
+        AccountService accountService = new AccountService();
+        User user = accountService.getByEmail(email);
+        
+        String filename = user.getPhotoPath();
+        // retrieve mimeType dynamically
+        String mime = cntx.getMimeType(filename);
+        if (mime == null) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            return;
+        }
+
+        response.setContentType(mime);
+        File file = new File(filename);
+        response.setContentLength((int) file.length());
+
+        FileInputStream in = new FileInputStream(file);
+        OutputStream out = response.getOutputStream();
+
+        // Copy the contents of the file to the output stream
+        byte[] buf = new byte[1024];
+        int count = 0;
+        while ((count = in.read(buf)) >= 0) {
+            out.write(buf, 0, count);
+        }
+        out.close();
+        in.close();
+
+    }  catch (Exception ex) {
+            Logger.getLogger(PictureServlet.class.getName()).log(Level.SEVERE, null, ex);
     }
+}
+
 
     /**
      * Handles the HTTP <code>POST</code> method.
@@ -67,19 +77,9 @@ public class PictureServlet extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
+    
 }
