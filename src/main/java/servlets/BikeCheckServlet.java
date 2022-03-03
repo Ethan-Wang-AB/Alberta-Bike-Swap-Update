1f2f0b79c6fd655201909f72a045fe9ba11b9399
845593 <845593@host.docker.internal>
2022-03-03 16:17:36 -0700

    add logic to bikecheck servlet

--- src/main/java/servlets/BikeCheckServlet.java
+++ src/main/java/servlets/BikeCheckServlet.java
@@ -11,14 +11,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.User;
+import services.AccountService;
 
 /**
  *
  * @author 845593
  */
 public class BikeCheckServlet extends HttpServlet {
-
-
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -32,37 +32,27 @@ public class BikeCheckServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        
-         String ticketnum = request.getParameter("ticketnum");
+
+        String ticketnum = request.getParameter("ticketnum");
         String phonenum = request.getParameter("phonenum");
-        
-        
-               
-        if(ticketnum == null || phonenum == null ){
+
+        if (ticketnum == null || phonenum == null) {
             request.setAttribute("message", "Both fields required");
             getServletContext().getRequestDispatcher("/WEB-INF/BikeCheckPage.jsp").forward(request, response);
-        }
-        
-        else if(ticketnum.equals("") || phonenum.equals("") ){
+        } else if (ticketnum.equals("") || phonenum.equals("")) {
             request.setAttribute("message", "Both fields required");
             request.getServletContext().getRequestDispatcher("/WEB-INF/BikeCheckPage.jsp").forward(request, response);
-        }
-        
-        else if (ticketnum != null && !ticketnum.equals("") && phonenum!= null && !phonenum.equals("")  ){
-            
+        } else if (ticketnum != null && !ticketnum.equals("") && phonenum != null && !phonenum.equals("")) {
+
             //Addtional check for the both password and ticket from database is require 
             //Start checking data here
             //Method
-            
-            
-            
             //If correct, forward to the EditBikePage for checking Bike status, ticket number also get forward
             request.setAttribute("ticketnum", ticketnum);
-           getServletContext().getRequestDispatcher("/WEB-INF/EditBikePage.jsp").forward(request, response);
-            }
-                   
-       // getServletContext().getRequestDispatcher("/WEB-INF/BikeCheckPage.jsp").forward(request, response);
+            getServletContext().getRequestDispatcher("/WEB-INF/editBikePage.jsp").forward(request, response);
+        }
+
+        // getServletContext().getRequestDispatcher("/WEB-INF/BikeCheckPage.jsp").forward(request, response);
     }
 
     /**
@@ -76,8 +66,33 @@ public class BikeCheckServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-    }
+        String ticket = request.getParameter("ticketnum");
+        String phone = request.getParameter("phonenum");
+        Long phoneNum;
+        AccountService accountService = AccountService.getInstance();
+        User user = new User();
+        String message = "ticket number is not exists";
+        try {
+            user = accountService.getByTicket(Integer.parseInt(ticket));
+            message = "ticket number exists;";
+            phoneNum = Long.parseLong(phone);
+        } catch (Exception e) {
+            if (user != null) {
+                message = message + "phone number is not in correct format";
 
-    
+            }
+            user = null;
+        }
+
+        if (user == null) {
+            request.setAttribute("message",message);
+            getServletContext().getRequestDispatcher("/WEB-INF/BikeCheckPage.jsp").forward(request, response);
+        }else{
+            request.setAttribute("bikes", user.getBikeList());
+            getServletContext().getRequestDispatcher("/WEB-INF/editBikePage.jsp").forward(request, response);
+
+        
+        }
+    }
 
 }
