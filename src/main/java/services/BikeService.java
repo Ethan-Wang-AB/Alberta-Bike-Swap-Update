cbc9a1f3bedde477522f6b8e5f03d73d1caadd2d
845593 <845593@host.docker.internal>
2022-03-17 15:32:43 -0600

    add getALl function and update photo function

--- src/main/java/services/BikeService.java
+++ src/main/java/services/BikeService.java
@@ -6,7 +6,15 @@
 package services;
 
 import dataaccess.BikeDB;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
+import java.sql.Blob;
+import java.util.ArrayList;
 import java.util.List;
+import javax.servlet.http.Part;
 import models.Bike;
 import models.EventDate;
 import models.User;
@@ -22,7 +30,7 @@ public class BikeService {
     private BikeService() {
     }
 
-    static BikeService getInstance() {
+   public static BikeService getInstance() {
         return bikeService;
     }
     BikeDB bikeDB = BikeDB.getInstance();
@@ -42,7 +50,7 @@ public class BikeService {
         bikeDB.delete(bike);
     }
 
-    void assignEventId(User user, EventDate event_date) {
+    public void assignEventId(User user, EventDate event_date) {
         List<Bike> bikes = user.getBikeList();
 
         for (int i = 1; i <= user.getBikeList().size(); i++) {
@@ -50,5 +58,24 @@ public class BikeService {
             bikeDB.update(bikes.get(i - 1));
 
         }
+    }
+    
+    public void updatePhoto(Bike bike, Part filePart) throws IOException{
+         if (filePart != null) {
+             
+                InputStream fileContent = filePart.getInputStream();
+                byte[] image = new byte[fileContent.available()];
+               fileContent.read(image);
+
+              //FileOutputStream writer = new FileOutputStream(file);
+               //writer.write(image);
+               bike.setPhoto(image);
+                  
+    }
+         updateBike(bike);
+    }
+    
+    public List<Bike> getAll(){
+    return bikeDB.getAll();
     }
 }
