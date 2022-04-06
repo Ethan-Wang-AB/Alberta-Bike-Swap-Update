1092d5b46618187622324451b9d67a83795f2772
845593 <845593@SAIT227780IT.ACDM.DS.SAIT.CA>
2022-04-05 21:02:53 -0600

    add report for user list

--- src/main/java/servlets/ReportServlet.java
+++ src/main/java/servlets/ReportServlet.java
@@ -5,20 +5,31 @@
  */
 package servlets;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import services.AccountService;
+import services.ReportService;
 
 /**
  *
  * @author 845593
  */
 public class ReportServlet extends HttpServlet {
-
-   
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -32,7 +43,55 @@ public class ReportServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-                    getServletContext().getRequestDispatcher("/WEB-INF/ReportPage.jsp").forward(request, response);
+            String path = getServletContext().getRealPath("/WEB-INF");
+           // System.out.println(path);
+//                 
+            String filename = path + "\\report" + ".xlsx";
+            //System.out.println(filename);
+            File file = new File(filename);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            OutputStream out = new FileOutputStream(file);
+            ReportService reportService = new ReportService();
+            List<String[]> data = reportService.userReport();
+            System.out.println(data.size());
+            System.out.println(data.get(0)[0]);
+            System.out.println(data.get(1)[0]);
+            System.out.println(data.get(2)[0]);
+            try {
+                XSSFWorkbook workbook = new XSSFWorkbook();
+                XSSFSheet sheet = workbook.createSheet();
+                for (int i = 0; i < data.size(); i++) {
+                    XSSFRow excelRow = sheet.createRow(i);
+                    String[] dbRow = data.get(i);
+                    for (int j = 0; j < dbRow.length; j++) {
+                        XSSFCell cell = excelRow.createCell(j);
+                        cell.setCellValue(dbRow[j]);
+
+                    }
+                }
+                ByteArrayOutputStream outByteStream = new ByteArrayOutputStream();
+                workbook.write(outByteStream);
+                byte[] outArray = outByteStream.toByteArray();
+
+                response.setContentType("application/vnd.ms-excel");
+                response.setContentLength(outArray.length);
+                response.setHeader("Content-Disposition", "attachment; filename=\""
+                        + filename + "\"");
+
+                OutputStream outStream = response.getOutputStream();
+                outStream.write(outArray);
+
+                outStream.flush();
+                outStream.close();
+
+            } catch (IOException ignore) {
+                ignore.printStackTrace();
+            } catch (Exception ex) {
+                Logger.getLogger(ReportServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+     
     }
 
     /**
@@ -48,6 +107,4 @@ public class ReportServlet extends HttpServlet {
             throws ServletException, IOException {
     }
 
-
- 
 }
