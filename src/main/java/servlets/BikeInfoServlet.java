ca515aeabb2d64c574bd220869923d84034c169d
851649 <851649@SAIT227671IT.ACDM.DS.SAIT.CA>
2022-04-14 14:31:38 -0600

    Adjusted the filter method to prevent issues with paging.

--- src/main/java/servlets/BikeInfoServlet.java
+++ src/main/java/servlets/BikeInfoServlet.java
@@ -39,16 +39,6 @@ public class BikeInfoServlet extends HttpServlet {
         BikeService bikeService = BikeService.getInstance();
         List<Bike> bikes=bikeService.getAll();
         int limit;
-        if(bikes.size() < 6){
-            limit = bikes.size();
-             request.setAttribute("nextDisplay", "none");
-        }
-        else{
-            limit = 6;
-        }
-        if(bikes.size() == 6){
-             request.setAttribute("nextDisplay", "none");
-        }
         // Price filter
         double minPrice=0;
         double maxPrice=99999;
@@ -80,15 +70,32 @@ public class BikeInfoServlet extends HttpServlet {
             index = request.getParameter("filterModel");
         }
         
-        //build list
-        ArrayList<Bike> filter=new ArrayList<>();
-        for(int i = 0 ; i < limit; i++){
-            Bike temp = bikes.get(i);
-            if(temp.getPrice()>=minPrice && temp.getPrice()<=maxPrice && temp.getBikeIndex().contains(index)
-                    && temp.getMaker().contains(make) && temp.getModel().contains(model)){
-                        filter.add(temp);
+       //Create a filtered array
+            ArrayList<Bike> filtered = new ArrayList<>();
+            for(int i = 0 ; i < bikes.size(); i++){
+                Bike temp = bikes.get(i);
+                if(temp.getPrice()>=minPrice && temp.getPrice()<=maxPrice && temp.getBikeIndex().contains(index)
+                        && temp.getMaker().contains(make) && temp.getModel().contains(model)){
+                            filtered.add(bikes.get(i));
+                }
             }
+        
+            //set up the limit of bikes to show based on the filtered array
+        if(filtered.size() <= 6){
+            limit = filtered.size();
+             request.setAttribute("nextDisplay", "none");
         }
+        else{
+            limit = 6;
+        }
+        
+        //build array that is actually displayed using all the above info
+            ArrayList<Bike> filter=new ArrayList<>();
+            for(int i = 0 ; i < limit; i++){
+                Bike temp = filtered.get(i);
+                filter.add(temp);
+            }
+
         request.setAttribute("bikes", filter);
         request.setAttribute("page", "0");
         request.setAttribute("prevDisplay", "none");
@@ -108,9 +115,11 @@ public class BikeInfoServlet extends HttpServlet {
             throws ServletException, IOException {
         BikeService bikeService = BikeService.getInstance();
         String action = request.getParameter("action");
+        List<Bike> bikes=bikeService.getAll();  
         //get filter information
         double minPrice=0;
         double maxPrice=9999;
+        
         try{
                 if(request.getParameter("minPrice") != null && request.getParameter("minPrice").length() > 0){
                 minPrice=Double.parseDouble(request.getParameter("minPrice"));
@@ -140,59 +149,73 @@ public class BikeInfoServlet extends HttpServlet {
             index = request.getParameter("filterModel");
         }
         
+        //Create a filtered array
+            ArrayList<Bike> filtered = new ArrayList<>();
+            for(int i = 0 ; i < bikes.size(); i++){
+                Bike temp = bikes.get(i);
+                if(temp.getPrice()>=minPrice && temp.getPrice()<=maxPrice && temp.getBikeIndex().contains(index)
+                        && temp.getMaker().contains(make) && temp.getModel().contains(model)){
+                            filtered.add(bikes.get(i));
+                }
+            }
+            
+            if(filtered.size() <= 6){
+                request.setAttribute("nextDisplay", "none");
+            }
+            //use filtered in the actions. 
         //someone bought a bike
         if(action != null && action.contains("Buy")){
             String bikeId = action.substring(3);
             bikeService.deleteBike(bikeService.getBike(Integer.parseInt(bikeId)));
             request.setAttribute("message","Purchase complete!");
             //load page with bikes after purchase
-            List<Bike> bikes=bikeService.getAll();  
+            
             int limit;
-            if(bikes.size() < 6){
-                limit = bikes.size();
-                request.setAttribute("nextDisplay", "none");
+            if(filtered.size() < 6){
+                limit = filtered.size();
+                request.setAttribute("prevDisplay", "none");
             }
             else{
                 limit = 6;
             }
-             if(bikes.size() == 6){
+             if(filtered.size() == 6){
              request.setAttribute("nextDisplay", "none");
         }
-            ArrayList<Bike> filter=new ArrayList<>();
+             //need to create a filtered array
+            ArrayList<Bike> result = new ArrayList<>();
             for(int i = 0 ; i < limit; i++){
             Bike temp = bikes.get(i);
             if(temp.getPrice()>=minPrice && temp.getPrice()<=maxPrice && temp.getBikeIndex().contains(index)
                     && temp.getMaker().contains(make) && temp.getModel().contains(model)){
-                filter.add(bikes.get(i));
+                result.add(bikes.get(i));
             }
         }
-        request.setAttribute("bikes", filter);
+            
+        if(result.size() <= 6){
+            request.setAttribute("nextDisplay", "none");
+        }
+        request.setAttribute("bikes", result);
         request.setAttribute("page", "0");
         request.setAttribute("prevDisplay", "none");
         getServletContext().getRequestDispatcher("/WEB-INF/BikeInfoPage.jsp").forward(request, response);
         }
         //otherwise, we are moving to another page.
-        List<Bike> bikes=bikeService.getAll();
         int limit;
         //see if we're moving to the next/previous page
         if(action.contains("Next")){
             ArrayList<Bike> bikeList = new ArrayList<>();
             int page = Integer.parseInt(action.substring(4)) + 6;
-            if(page + 6 < bikes.size()){
+            if(page + 6 < filtered.size()){
                 limit = page + 6;
             }
             else{
-                limit = bikes.size();
+                limit = filtered.size();
                 request.setAttribute("listMessage","You have reached the end of our selection!");
                 request.setAttribute("nextDisplay", "none");
             }
             for(int i = page; i < limit ; i++){
-            Bike temp = bikes.get(i);
-            if(temp.getPrice()>=minPrice && temp.getPrice()<=maxPrice && temp.getBikeIndex().contains(index)
-                    && temp.getMaker().contains(make) && temp.getModel().contains(model)){
-                bikeList.add(bikes.get(i));
-            }
-                    page++;
+                Bike temp = filtered.get(i);
+                bikeList.add(temp);
                 }
             request.setAttribute("bikes", bikeList);
             request.setAttribute("page", page);
@@ -203,21 +226,18 @@ public class BikeInfoServlet extends HttpServlet {
             ArrayList<Bike> bikeList=new ArrayList<>();
             int page = Integer.parseInt(action.substring(4));
             limit = page - 1;
-            if(page - 7 <= 0){
+            if(page -  6<= 0){
                 page = 0;
                 //request.setAttribute("listMessage","You have reached the end of our selection!");
                 request.setAttribute("prevDisplay", "none");
             }
             else{
-                page = page - 7;
+                page = page - 6;
             }
-            for(int i = page; i < limit; i++){
-            Bike temp = bikes.get(i);
-            if(temp.getPrice()>=minPrice && temp.getPrice()<=maxPrice && temp.getBikeIndex().contains(index)
-                    && temp.getMaker().contains(make) && temp.getModel().contains(model)){
-                bikeList.add(bikes.get(i));
-            }
-                } 
+            for(int i = page; i <= limit; i++){
+                Bike temp = filtered.get(i);
+                bikeList.add(temp);
+            } 
             request.setAttribute("bikes", bikeList);
             request.setAttribute("page", page);
             getServletContext().getRequestDispatcher("/WEB-INF/BikeInfoPage.jsp").forward(request, response);
